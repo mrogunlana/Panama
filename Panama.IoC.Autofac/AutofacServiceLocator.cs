85e645e0ab97112c8751af41be799bbd0972ceeb
mrogunlana <diran.ogunlana@gmail.com>
2019-05-02 23:20:45 -0400

    adding ioc (autofac) port

---
+++ Panama.IoC.Autofac/AutofacServiceLocator.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+
+namespace Panama.IoC.Autofac
+{
+    public class AutofacServiceLocator : IServiceLocator
+    {
+        private static IContainer _kernel = null;
+
+        public AutofacServiceLocator(IContainer kernel)
+        {
+            _kernel = kernel;
+        }
+
+        public T Resolve<T>()
+        {
+            return _kernel.Resolve<T>();
+        }
+
+        public T Resolve<T>(string name)
+        {
+            return _kernel.ResolveNamed<T>(name);
+        }
+    }
+}
---
+++ Panama.IoC.Autofac/DefaultAutofacConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using Autofac.Features.AttributeFilters;
+using Panama.Commands;
+using System;
+using System.Linq;
+
+namespace Panama.IoC.Autofac
+{
+    public class DefaultAutofacConfiguration
+    {
+        public ContainerBuilder Register(ContainerBuilder builder)
+        {
+            //Register all validators -- singletons
+            builder.RegisterAssemblyTypes(AppDomain.CurrentDomain.GetAssemblies())
+                   .Where(t => t.IsAssignableTo<IValidation>())
+                   .Named<IValidation>(t => t.Name)
+                   .AsImplementedInterfaces()
+                   .SingleInstance();
+
+            //Register all commands -- singletons
+            builder.RegisterAssemblyTypes(AppDomain.CurrentDomain.GetAssemblies())
+                   .Where(t => t.IsAssignableTo<ICommand>())
+                   .Named<ICommand>(t => t.Name)
+                   .AsImplementedInterfaces()
+                   .SingleInstance()
+                   .WithAttributeFiltering();
+
+            return builder;
+        }
+    }
+}
