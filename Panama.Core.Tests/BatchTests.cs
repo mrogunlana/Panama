f73b406f9a5fd6c2d4cf40c37b677a66d7f05128
mrogunlana <diran.ogunlana@gmail.com>
2019-08-23 10:36:38 -0400

    updated mysql test to use environment variable instead of app.config

--- Panama.Core.Tests/BatchTests.cs
+++ Panama.Core.Tests/BatchTests.cs
@@ -21,6 +21,12 @@ namespace Panama.Core.Tests
         [ClassInitialize]
         public static void ClassInit(TestContext context)
         {
+            Environment.SetEnvironmentVariable("ASPNETCORE_MYSQL_SERVER", "localhost", EnvironmentVariableTarget.Process);
+            Environment.SetEnvironmentVariable("ASPNETCORE_MYSQL_PORT", "3309", EnvironmentVariableTarget.Process);
+            Environment.SetEnvironmentVariable("ASPNETCORE_MYSQL_DATABASE", "panama-core", EnvironmentVariableTarget.Process);
+            Environment.SetEnvironmentVariable("ASPNETCORE_MYSQL_USER", "panama-db", EnvironmentVariableTarget.Process);
+            Environment.SetEnvironmentVariable("ASPNETCORE_MYSQL_PASSWORD", "abc123", EnvironmentVariableTarget.Process);
+
             var builder = new ContainerBuilder();
             builder.RegisterType<SqlGeneratorImpl>()
                .As<ISqlGenerator>()
