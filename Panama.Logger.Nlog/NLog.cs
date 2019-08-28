a988b592c15322a204dde6e0dbe6ac3f00bc77e6
mrogunlana <diran.ogunlana@gmail.com>
2019-08-27 20:49:47 -0400

    I'm currently thinking of a way to get an NLog file per IHandler instance... any ideas welcomed... :)

--- Panama.Logger.Nlog/NLog.cs
+++ Panama.Logger.Nlog/NLog.cs
@@ -5,8 +5,23 @@ namespace Panama.Core.Logger
 {
     public class NLog : ILog
     {
+        //private readonly Guid _id;
+
+        //public NLog()
+        //{
+        //    _id = new Guid();
+        //}
         public void LogException<T>(Exception obj)
         {
+            //TODO: Maybe this could work in conjunction with Autofac scoping
+            //to provide Log per Handler Request??
+            //var log = NLogger.LogManager.GetLogger(obj.GetType().FullName);
+            //var info = new NLogger.LogEventInfo(NLogger.LogLevel.Error, log.Name, obj.Message);
+
+            //info.Properties["LogId"] = _id.ToString();
+
+            //log.Log(info);
+
             var log = NLogger.LogManager.GetLogger(obj.GetType().FullName);
 
             log.Log(NLogger.LogLevel.Error, obj);
