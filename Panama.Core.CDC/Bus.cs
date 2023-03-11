393d3d4f39bca418da8f81ba02dfedac425108b6
Diran Ogunlana <diran.ogunlana@gmail.com>
2023-03-11 12:34:11 -0500

    [WIP] adding basic IBus impl

---
+++ Panama.Core.CDC/Bus.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.Extensions.Logging;
+using Panama.Core.CDC.Extensions;
+using Panama.Core.CDC.Interfaces;
+using Panama.Core.CDC.Models;
+using Panama.Core.Interfaces;
+
+namespace Panama.Core.CDC
+{
+    public class Bus : IBus
+    {
+        private readonly ILogger<Bus> _log;
+        private readonly IDispatcher _dispatcher;
+        public IServiceProvider ServiceProvider { get; }
+        public AsyncLocal<ITransaction> Transaction { get; }
+
+        public Bus(
+              IServiceProvider provider
+            , IDispatcher dispatcher
+            , ILogger<Bus> log)
+        {
+            _log = log;
+            _dispatcher = dispatcher;
+
+            ServiceProvider = provider;
+            Transaction = new AsyncLocal<ITransaction>();
+        }
+
+        public async Task PublishAsync<D>(string name
+            , D? data
+            , string? ack = null
+            , string? nack = null
+            , CancellationToken cancellationToken = default
+            , string correlationId = "") 
+            where D : IModel
+        {
+            var message = new Message()
+                .AddCorrelationId(correlationId)
+                .AddData(data)
+                .AddAck(ack)
+                .AddNack(nack)
+                .ToInternal(ServiceProvider);
+
+            await _dispatcher.Publish(message).ConfigureAwait(false);
+        }
+
+        public async Task PublishAsync<D>(string name
+            , D? data
+            , IDictionary<string, string?> headers
+            , string? ack = null
+            , string? nack = null
+            , CancellationToken cancellationToken = default
+            , string correlationId = "") 
+            where D : IModel
+        {
+            var message = new Message()
+                .AddCorrelationId(correlationId)
+                .AddHeaders(headers)
+                .AddData(data)
+                .AddAck(ack)
+                .AddNack(nack)
+                .ToInternal(ServiceProvider);
+
+            await _dispatcher.Publish(message).ConfigureAwait(false);
+        }
+
+        public async Task PublishAsync<D, T>(string name
+            , D? data
+            , IDictionary<string, string?> headers
+            , string? ack = null
+            , string? nack = null
+            , CancellationToken cancellationToken = default
+            , string correlationId = "")
+            where D : IModel
+            where T : ITarget
+        {
+            var message = new Message()
+                .AddCorrelationId(correlationId)
+                .AddHeaders(headers)
+                .AddMessageGroup(nameof(T))
+                .AddData(data)
+                .AddAck(ack)
+                .AddNack(nack)
+                .ToInternal(ServiceProvider);
+
+            await _dispatcher.Publish(message).ConfigureAwait(false);
+        }
+
+        public async Task PublishDelayAsync<D>(TimeSpan delay
+            , string name
+            , D? data
+            , string? ack = null
+            , string? nack = null
+            , CancellationToken cancellationToken = default
+            , string correlationId = "") 
+            where D : IModel
+        {
+            var message = new Message()
+                .AddCorrelationId(correlationId)
+                .AddDelayTime(delay)
+                .AddData(data)
+                .AddAck(ack)
+                .AddNack(nack)
+                .ToInternal(ServiceProvider);
+
+            await _dispatcher.Publish(message).ConfigureAwait(false);
+        }
+
+        public async Task PublishDelayAsync<D>(TimeSpan delay
+            , string name
+            , D? data
+            , IDictionary<string, string?> headers
+            , string? ack = null
+            , string? nack = null
+            , CancellationToken cancellationToken = default
+            , string correlationId = "") 
+            where D : IModel
+        {
+            var message = new Message()
+                .AddCorrelationId(correlationId)
+                .AddHeaders(headers)
+                .AddDelayTime(delay)
+                .AddData(data)
+                .AddAck(ack)
+                .AddNack(nack)
+                .ToInternal(ServiceProvider);
+
+            await _dispatcher.Publish(message).ConfigureAwait(false);
+        }
+
+        public async Task PublishDelayAsync<D, T>(TimeSpan delay
+            , string name
+            , D? data
+            , IDictionary<string, string?> headers
+            , string? ack = null
+            , string? nack = null
+            , CancellationToken cancellationToken = default
+            , string correlationId = "")
+            where D : IModel
+            where T : ITarget
+        {
+            var message = new Message()
+                .AddCorrelationId(correlationId)
+                .AddHeaders(headers)
+                .AddMessageGroup(nameof(T))
+                .AddDelayTime(delay)
+                .AddData(data)
+                .AddAck(ack)
+                .AddNack(nack)
+                .ToInternal(ServiceProvider);
+
+            await _dispatcher.Publish(message).ConfigureAwait(false);
+        }
+    }
+}
--- Panama.Core.CDC/Interfaces/IBus.cs
+++ Panama.Core.CDC/Interfaces/IBus.cs
@@ -12,7 +12,8 @@ namespace Panama.Core.CDC.Interfaces
             , D? data
             , string? ack = null
             , string? nack = null
-            , CancellationToken cancellationToken = default)
+            , CancellationToken cancellationToken = default
+            , string correlationId = "")
             where D : IModel;
 
         Task PublishAsync<D>(string name
@@ -20,7 +21,8 @@ namespace Panama.Core.CDC.Interfaces
             , IDictionary<string, string?> headers
             , string? ack = null
             , string? nack = null
-            , CancellationToken cancellationToken = default)
+            , CancellationToken cancellationToken = default
+            , string correlationId = "")
             where D : IModel;
 
         Task PublishAsync<D, T>(string name
@@ -28,34 +30,38 @@ namespace Panama.Core.CDC.Interfaces
             , IDictionary<string, string?> headers
             , string? ack = null
             , string? nack = null
-            , CancellationToken cancellationToken = default)
+            , CancellationToken cancellationToken = default
+            , string correlationId = "")
             where D : IModel
             where T : ITarget;
 
-        Task PublishDelayAsync<D>(TimeSpan delayTime
+        Task PublishDelayAsync<D>(TimeSpan delay
             , string name
             , D? data
             , string? ack = null
             , string? nack = null
-            , CancellationToken cancellationToken = default)
+            , CancellationToken cancellationToken = default
+            , string correlationId = "")
             where D : IModel;
 
-        Task PublishDelayAsync<D>(TimeSpan delayTime
+        Task PublishDelayAsync<D>(TimeSpan delay
             , string name
             , D? data
             , IDictionary<string, string?> headers
             , string? ack = null
             , string? nack = null
-            , CancellationToken cancellationToken = default)
+            , CancellationToken cancellationToken = default
+            , string correlationId = "")
             where D : IModel;
 
-        Task PublishDelayAsync<D, T>(TimeSpan delayTime
+        Task PublishDelayAsync<D, T>(TimeSpan delay
             , string name
             , D? data
             , IDictionary<string, string?> headers
             , string? ack = null
             , string? nack = null
-            , CancellationToken cancellationToken = default)
+            , CancellationToken cancellationToken = default
+            , string correlationId = "")
             where D : IModel
             where T : ITarget;
     }
