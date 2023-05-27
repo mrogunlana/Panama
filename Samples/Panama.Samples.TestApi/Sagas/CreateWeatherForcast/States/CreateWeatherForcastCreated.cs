72a32394c2aa318bce4c32ba4e7ca5f9cb8611ee
Diran Ogunlana <diran.ogunlana@gmail.com>
2023-05-27 15:42:28 -0400

    removed TestApi as its deprecated in favor of the broker/store specific samples

--- Samples/Panama.Samples.TestApi/Sagas/CreateWeatherForcast/States/CreateWeatherForcastCreated.cs
+++
@@ -1,6 +0,0 @@
-﻿using Panama.Canal.Sagas.Stateless.Interfaces;
-
-namespace Panama.Samples.TestApi.Sagas.CreateWeatherForcast.States
-{
-    public class CreateWeatherForcastCreated : ISagaState { }
-}
